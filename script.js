adbcd7afc74b9e48b5d7809ec7780774d7798a8c
Hugo9A <152261752+Hugo9A@users.noreply.github.com>
2025-02-07 10:38:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function showPage(pageId) {
-    // Hide all sections
     const pages = document.querySelectorAll('.page');
     pages.forEach(page => page.style.display = 'none');
 
-    // Show the selected page
     const activePage = document.getElementById(pageId);
     activePage.style.display = 'block';
 }
 
-// Initially show the home page
 window.onload = () => showPage('home');
+
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('carousel-track')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
